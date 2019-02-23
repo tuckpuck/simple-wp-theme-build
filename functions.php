e70793d2e66892af3f462c0c722470255cf93a83
tuckpuck <tucker.triggs@yahoo.com>
2019-02-22 18:30:52 -0700

    added and linked custom stylesheet and javascript file

--- functions.php
+++ functions.php
@@ -2,10 +2,35 @@
 
 function load_stylesheets() 
 {
-    wp_register_style('stylesheet', get_template_directory_uri() . "/css/bootstrap.min.css", array(), false, 'all');
-    wp_enqueue_style('stylesheet');
+    wp_register_style('bootstrap', get_template_directory_uri() . "/css/bootstrap.min.css", array(), false, 'all');
+    wp_enqueue_style('bootstrap');
+
+
+    wp_register_style('style', get_template_directory_uri() . "/css/style.css", array(), false, 'all');
+    wp_enqueue_style('style');
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+
+function load_js() 
+{
+    wp_register_script('customjs', get_template_directory_uri() . "/js/scripts.js", '', 1, true);
+    wp_enqueue_script('customjs');
+
+}
+
+add_action('wp_enqueue_scripts', 'load_js');
+
+
+
+
+
+
+
+
+
+
 
 ?>
