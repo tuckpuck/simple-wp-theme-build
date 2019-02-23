a55787bb45ed6e607e3b1ea4470380cf8c1bb56e
tuckpuck <tucker.triggs@yahoo.com>
2019-02-23 13:10:56 -0700

    dynamically setting content on front page

--- front-page.php
+++ front-page.php
@@ -1,3 +1,14 @@
 <?php get_header();?>
-<div class="text-center">hello world</div> 
+<div class="container">
+<div class="row">
+<div class="text-center py-5 display-3 mx-auto"><?php the_title();?></div>
+
+<?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+<?php the_content();?>
+
+<?php endwhile; endif;?>
+
+</div>
+</div>
 <?php get_footer();?>
