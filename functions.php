7ad58e08df8c5d5b81cd6be8a910cccf203a8716
tuckpuck <tucker.triggs@yahoo.com>
2019-02-22 18:44:25 -0700

    added jquery script

--- functions.php
+++ functions.php
@@ -12,6 +12,13 @@ function load_stylesheets()
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
+function load_jquery() 
+{
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', get_template_directory_uri() . "/js/jquery-3.3.1.min.js", '', 1, true);
+    wp_enqueue_script('jquery');
+}
+add_action('wp_enqueue_scripts', 'load_jquery');
 
 
 function load_js() 
@@ -19,8 +26,8 @@ function load_js()
     wp_register_script('customjs', get_template_directory_uri() . "/js/scripts.js", '', 1, true);
     wp_enqueue_script('customjs');
 
-}
 
+}
 add_action('wp_enqueue_scripts', 'load_js');
 
 
