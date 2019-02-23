15bf516c56e2f84f5d364a7f0b1a32bb02adadb7
tuckpuck <tucker.triggs@yahoo.com>
2019-02-22 17:04:22 -0700

    head and body tags added with php

---
+++ front-page.php
@@ -0,0 +1,3 @@
+<?php get_header();?>
+hello world
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php wp_head();?>
+</head>
+<body <?php body_class();?>>
+    
